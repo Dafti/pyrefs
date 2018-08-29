8f4ca83297301bef8e10e80783a09414f1d9f427
Dafti <daniel.gracia-perez@cfa-afti.fr>
2018-08-29 11:08:28 +0200

    [Dev] Simplified mmls arguments

--- mmls.py
+++ mmls.py
@@ -4,15 +4,15 @@ import media.gpt as gpt
 
 def mmls():
     parser = argparse.ArgumentParser(description='Read MBR from provided dump.')
-    parser.add_argument('dump', action='store', nargs=1,
+    parser.add_argument('dump', action='store',
             type=argparse.FileType(mode='rb'))
     args = parser.parse_args()
-    mbr_data = mbr.readMBR(args.dump[0])
+    mbr_data = mbr.readMBR(args.dump)
     print(mbr_data)
     # we make the supposition that if it is a GPT media then only one
     # partition is declared in the MBR
     if len(mbr_data) == 1 and mbr_data[0][1]['ptype'] == mbr.MBR_PARTTYPE_GPT:
-        gpt_data = gpt.readGPT(args.dump[0], mbr_data[0][1]['start'])
+        gpt_data = gpt.readGPT(args.dump, mbr_data[0][1]['start'])
         print(gpt_data)
 
 if __name__ == '__main__':
