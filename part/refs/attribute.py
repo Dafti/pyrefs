bd5d44af540830a62ae7d720dd01a5906f13213a
Dafti <daniel.gracia-perez@cfa-afti.fr>
2018-08-30 15:52:57 +0200

    [Dev] Modified attribute parsing

--- part/refs/attribute.py
+++ part/refs/attribute.py
@@ -5,38 +5,112 @@ ATTR_SIZE_SIZE = 4
 ATTR_SIZE_FORMAT = Struct('<L')
 ATTR_TYPE_OFFSET = 0x10
 ATTR_TYPE_SIZE = 4
+ATTR_TYPE_FORMAT = Struct('<L')
 
 ATTR_HEADER_FORMAT = Struct('<LHHHHHHL')
+
+ATTR_FILENAME_HEADER_FORMAT = Struct('<LHHHHHHL')
+
+ATTR_FOLDER_HEADER_1_FORMAT = Struct('<L6sHLL')
+ATTR_FOLDER_HEADER_2_FORMAT = Struct('<Q2sQQQQ')
 
 ATTR_TYPE_FILENAME = 0x00010030
 ATTR_TYPE_FOLDER   = 0x00020030
 
-def read_filename_attribute(attr, dump, offset):
-    dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
-    fn = dump.read(attr['length'] - ATTR_TYPE_SIZE)
-    attr['filename'] = fn
-
-def read_folder_attribute(attr, dump, offset):
-    dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
-    fn = dump.read(attr['length'] - ATTR_TYPE_SIZE)
-    attr['foldername'] = fn
-
-def read_attribute(dump, offset):
+def read_filename_attribute(dump, offset):
     dump.seek(offset, 0)
-    data = dump.read(ATTR_HEADER_FORMAT.size)
-    data_fields = ATTR_HEADER_FORMAT.unpack_from(data, 0)
-    attr_size = data_fields[0]
-    attr = {'size': data_fields[0],
+    data = dump.read(ATTR_FILENAME_HEADER_FORMAT.size)
+    data_fields = ATTR_FILENAME_HEADER_FORMAT.unpack_from(data, 0)
+    attr = {'absolute_offset': offset,
+            'size': data_fields[0],
             'header_length': data_fields[1],
             'length': data_fields[2],
             'next_struct_offset': data_fields[4],
             'record_rem_data': data_fields[5],
             'type': data_fields[7]}
-    if attr['type'] == ATTR_TYPE_FILENAME:
-        read_filename_attribute(attr, dump, offset)
-    if attr['type'] == ATTR_TYPE_FOLDER:
-        read_folder_attribute(attr, dump, offset)
+    f_size = attr['length'] - ATTR_TYPE_SIZE
+    if f_size > 0:
+        dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
+        fn = dump.read(f_size)
+    else:
+        fn = 'DEADBEEF'.encode('utf-16le')
+    attr['filename'] = fn
     return attr
+
+# def read_filename_attribute(attr, dump, offset):
+#     dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
+#     # if we are sure that we are in a filename attribute we could
+#     # remove this test
+#     size = attr['length'] - ATTR_TYPE_SIZE
+#     if size > 0:
+#         fn = dump.read(attr['length'] - ATTR_TYPE_SIZE)
+#     else:
+#         fn = 'DEADBEEF'.encode('utf-16le')
+#     attr['filename'] = fn
+
+def read_folder_attribute(dump, offset):
+    dump.seek(offset, 0)
+    data = dump.read(ATTR_FOLDER_HEADER_1_FORMAT.size)
+    data_fields = ATTR_FOLDER_HEADER_1_FORMAT.unpack_from(data, 0)
+    attr = {'absolute_offset': offset,
+            'size': data_fields[0],
+            'end_name_offset': data_fields[2],
+            'record_rem_data': data_fields[3],
+            'type': data_fields[4]}
+    f_size = attr['end_name_offset'] - (ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE)
+    if f_size > 0:
+        dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
+        fn = dump.read(f_size)
+    else:
+        fn = 'DEADBEEF'.encode('utf-16le')
+    attr['foldername'] = fn
+    dump.seek(offset + attr['end_name_offset'], 0)
+    data = dump.read(ATTR_FOLDER_HEADER_2_FORMAT.size)
+    data_fields = ATTR_FOLDER_HEADER_2_FORMAT.unpack_from(data, 0)
+    attr['nodeid'] = data_fields[0]
+    attr['created'] = data_fields[2]
+    attr['modified'] = data_fields[3]
+    attr['metadata_modified'] = data_fields[4]
+    attr['last_accessed'] = data_fields[5]
+    return attr
+
+# def read_folder_attribute(attr, dump, offset):
+#     dump.seek(offset + ATTR_TYPE_OFFSET + ATTR_TYPE_SIZE, 0)
+#     # if we are sure that we are in a foldername attribute we could
+#     # remove this test
+#     size = attr['length'] - ATTR_TYPE_SIZE
+#     if size > 0:
+#         fn = dump.read(attr['length'] - ATTR_TYPE_SIZE)
+#     else:
+#         fn = 'BEEFDEAD'.encode('utf-16le')
+#     attr['foldername'] = fn
+
+def read_attribute(dump, offset):
+    dump.seek(offset + ATTR_TYPE_OFFSET, 0)
+    data = dump.read(ATTR_TYPE_FORMAT.size)
+    attr_type, = ATTR_TYPE_FORMAT.unpack_from(data,0)
+    if attr_type == ATTR_TYPE_FILENAME:
+        attr = read_filename_attribute(dump, offset)
+    if attr_type == ATTR_TYPE_FOLDER:
+        attr = read_folder_attribute(dump, offset)
+    return attr
+
+# def read_attribute(dump, offset):
+#     dump.seek(offset, 0)
+#     data = dump.read(ATTR_HEADER_FORMAT.size)
+#     data_fields = ATTR_HEADER_FORMAT.unpack_from(data, 0)
+#     attr = {'absolute_offset': offset,
+#             'size': data_fields[0],
+#             'header_length': data_fields[1],
+#             'length': data_fields[2],
+#             'next_struct_offset': data_fields[4],
+#             'record_rem_data': data_fields[5],
+#             'type': data_fields[7]}
+#     if attr['type'] == ATTR_TYPE_FILENAME:
+#         read_filename_attribute(attr, dump, offset)
+#     if attr['type'] == ATTR_TYPE_FOLDER:
+#         read_folder_attribute(attr, dump, offset)
+#     return attr
 
 def dump_attribute(attr):
     print(attr)
