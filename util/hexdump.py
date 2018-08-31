ce50f4e65b44d4816f3b33d638725a214f7cd7cb
Dafti <daniel.gracia-perez@cfa-afti.fr>
2018-08-31 08:42:33 +0200

    [Dev] Hexdump util handling of short lines

--- util/hexdump.py
+++ util/hexdump.py
@@ -1,13 +1,15 @@
 def hexdump(data, address=0, width=16):
     max_address = address + len(data)
     address_len = len('{:#x}'.format(max_address)) + 1
-    data_format = ' {:02x}' * width
-    line_format = '{:#0' + str(address_len) + 'x}:' + data_format + '    {}'
-    header_format = '{:<' + str(address_len) + '} ' + data_format + '    {}'
+    data_format = ' {:02x}'
+    text_format = '    {}'
+    header_format = '{:<' + str(address_len) + '} ' + (data_format * width) + text_format
     buf = [ x % 0x100 for x in range(width) ]
     print(header_format.format('', *buf, ''))
     for index in range(0, len(data), width):
         buf = data[index:index + width]
         dec = ''.join([ chr(x) if x >= 32 and x <= 126 else '.' for x in buf ])
+        white_format = '   ' * (width - len(buf))
+        line_format = '{:#0' + str(address_len) + 'x}:' + (data_format * len(buf)) + white_format + text_format
         print(line_format.format(address + index, *buf, dec))
 
