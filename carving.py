309c3d103033ca29af9d2e66ece7e99f912be9ca
Dafti <daniel.gracia-perez@cfa-afti.fr>
2018-08-31 08:51:59 +0200

    [Dev] Tree control and allocator carving

--- carving.py
+++ carving.py
@@ -1,6 +1,11 @@
 import argparse
+import sys
 from struct import Struct
+# import part.refs.entry_block as reb
+import part.refs.tree_control as rtc
 import part.refs.attribute as refs_attr
+import part.refs.allocator as alloc
+from util.hexdump import hexdump
 
 def find_bytes(entry, block):
     hits = []
@@ -20,12 +25,13 @@ def find_blocks(dump, offset, end, step):
         dump.seek(i, 0)
         data = dump.read(4)
         entryblock,=ENTRYBLOCK_FORMAT.unpack_from(data, 0)
-        if entryblock != 0:
+        if (i - offset) / step == entryblock:
+        # if entryblock != 0:
             dump.seek(i+nodeid_offset,0)
-            data = dump.read(2)
+            data = dump.read(4)
             nodeid, = NODEID_FORMAT.unpack_from(data, 0)
             dump.seek(i+childid_offset,0)
-            data = dump.read(2)
+            data = dump.read(4)
             childid, = NODEID_FORMAT.unpack_from(data, 0)
             dump.seek(i+counter_offset, 0)
             counter = dump.read(1)[0]
@@ -65,21 +71,23 @@ parser.add_argument('dump', action='store',
         type=argparse.FileType(mode='rb'))
 args = parser.parse_args()
 
-offset=0x8100000
-end=0x16d00000
+offset = 0x8100000
+end = (41940991*512)+512 # complete partition
+# end = offset + (20000 * 1024 * 16)
+# end = 0x16d34000
 step=16*1024
 counter_offset=0x8
 nodeid_offset=0x18
 childid_offset=0x20
-ENTRYBLOCK_FORMAT=Struct('<H')
-NODEID_FORMAT=Struct('<H')
+ENTRYBLOCK_FORMAT=Struct('<L')
+NODEID_FORMAT=Struct('<L')
 
 blocks = find_blocks(args.dump, offset, end, step)
 print("==================================================================")
-print("Blocks found")
+print("Blocks found ({})".format(len(blocks)))
 print("==================================================================")
 for b in blocks:
-    print('{:#010x} - {:#010x}: {:#06x} {:>3} {:#06x} {:#06x}'.format(b['offset'],
+    print('{:#010x} - {:#010x}: {:#010x} {:>3} {:#010x} {:#010x}'.format(b['offset'],
         b['offset'] + step, b['entryblock'], b['counter'], b['nodeid'], b['childid']))
 
 # blocks_with_files = blocks_with_file_attributes(args.dump, blocks, step)
@@ -90,7 +98,7 @@ for b in blocks:
 #     print('{:#010x} - {:#010x}: {:#06x} {:>3} {:#06x} {:#06x} {:4} {:4}'.format(b['offset'],
 #         b['offset'] + step, b['entryblock'], b['counter'], b['nodeid'], b['childid'],
 #         len(b['fileids']), len(b['folderids'])))
-# 
+#
 # print("==================================================================")
 # print("Files found")
 # print("==================================================================")
@@ -104,7 +112,7 @@ for b in blocks:
 #         print('{:#010x} {:#06x} {:#06x} {:#06x} {:#010x} {}'.format(
 #             block['offset'], block['entryblock'],
 #             block['nodeid'], block['childid'], attr['absolute_offset'], filename))
-# 
+#
 # print("==================================================================")
 # print("Folders found")
 # print("==================================================================")
@@ -118,24 +126,101 @@ for b in blocks:
 #         print('{:#010x} {:#06x} {:#06x} {:#06x} {:#010x} {}'.format(
 #             block['offset'], block['entryblock'],
 #             block['nodeid'], block['childid'], attr['absolute_offset'], foldername))
+#
+# blocks_with_child = blocks_with_child_attributes(args.dump, blocks, step)
+# print("==================================================================")
+# print("Blocks with child found")
+# print("==================================================================")
+# for b in blocks_with_child:
+#     print('{:#010x} - {:#010x}: {:#06x} {:>3} {:#06x} {:#06x} {:4}'.format(b['offset'],
+#         b['offset'] + step, b['entryblock'], b['counter'], b['nodeid'], b['childid'],
+#         len(b['childids'])))
+#     for oc in b['childids']:
+#         attr = refs_attr.read_attribute(args.dump, oc)
+#         if attr['000c'] != 0x000c:
+#             print('False positive')
+#         else:
+#             try:
+#                 attr['filename'] = attr['filename'].decode('utf-16le')
+#             except:
+#                 attr['filename'] = attr['filename']
+#             print(attr)
 
-blocks_with_child = blocks_with_child_attributes(args.dump, blocks, step)
-print("==================================================================")
-print("Blocks with child found")
-print("==================================================================")
-for b in blocks_with_child:
-    print('{:#010x} - {:#010x}: {:#06x} {:>3} {:#06x} {:#06x} {:4}'.format(b['offset'],
-        b['offset'] + step, b['entryblock'], b['counter'], b['nodeid'], b['childid'],
-        len(b['childids'])))
-    for oc in b['childids']:
-        attr = refs_attr.read_attribute(args.dump, oc)
-        if attr['000c'] != 0x000c:
-            print('False positive')
-        else:
-            try:
-                attr['filename'] = attr['filename'].decode('utf-16le')
-            except:
-                attr['filename'] = attr['filename']
-            print(attr)
+addr = offset + (16 * 1024 * 0x1e)
+tc = rtc.read_tree_control(args.dump, addr)
+print(tc)
+rtc.dump_tree_control(tc)
+args.dump.seek(addr, 0)
+data = args.dump.read(tc['tree_control_size'])
+hexdump(data, addr)
+
+# tree control copies
+# print('')
+#
+# addr = 0x4fc0f4000
+# tc = rtc.read_tree_control(args.dump, addr)
+# print(tc)
+# rtc.dump_tree_control(tc)
+# args.dump.seek(addr, 0)
+# data = args.dump.read(tc['tree_control_size'])
+# hexdump(data, addr)
+#
+# print('')
+#
+# addr = 0x4fc0f8000
+# tc = rtc.read_tree_control(args.dump, addr)
+# print(tc)
+# rtc.dump_tree_control(tc)
+# args.dump.seek(addr, 0)
+# data = args.dump.read(tc['tree_control_size'])
+# hexdump(data, addr)
+
+print('')
+
+tc_e = []
+addr = offset + (16 * 1024 * tc['extent_pointers'][0])
+tc_e.append(rtc.read_tree_control_ext(args.dump, addr))
+print(tc_e[0])
+rtc.dump_tree_control_ext(tc_e[0])
+args.dump.seek(addr, 0)
+data = args.dump.read(tc_e[0]['tree_control_ext_size'])
+hexdump(data, addr)
+
+print('')
+
+addr = offset + (16 * 1024 * tc['extent_pointers'][1])
+tc_e.append(rtc.read_tree_control_ext(args.dump, addr))
+print(tc_e[1])
+rtc.dump_tree_control_ext(tc_e[1])
+args.dump.seek(addr, 0)
+data = args.dump.read(tc_e[1]['tree_control_ext_size'])
+hexdump(data, addr)
+
+for i, rec in enumerate(tc_e[0]['records']):
+    print('')
+
+    eb_array = rec[0:2]
+    eb_num = eb_array[0] + (256 * eb_array[1])
+    print('{} entryblock number: {:#x}'.format(i, eb_num))
+    addr = offset + (16 * 1024 * eb_num)
+    args.dump.seek(addr, 0)
+    data = args.dump.read(512)
+    hexdump(data, addr)
+
+for i, rec in enumerate(tc_e[0]['records']):
+    if i not in [1, 2, 3]:
+        continue
+    print('')
+
+    eb_array = rec[0:2]
+    eb_num = eb_array[0] + (256 * eb_array[1])
+    addr = offset + (16 * 1024 * eb_num)
+    al = alloc.read_allocator(args.dump, addr)
+    print(al)
+    alloc.dump_allocator(al)
+    args.dump.seek(addr, 0)
+    # data = args.dump.read(al['_structure_size'])
+    data = args.dump.read(16 * 1024)
+    hexdump(data, addr)
 
 args.dump.close()
